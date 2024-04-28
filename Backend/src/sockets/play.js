db1e8e09aa0de2b865f12bb7f136c8bf02d77646
sushant-wayal <sushantssr12345@gmail.com>
2024-04-28 20:24:52 +0530

    updated join event

--- Backend/src/sockets/play.js
+++ Backend/src/sockets/play.js
@@ -1,31 +1,22 @@
 import { io } from "../app.js";
 
-const finder = [];
-const waiter = [];
+let waiting = null;
 
 const play = () => {
     io.on("connection", (socket) => {
         socket.on("join", async () => {
-            if (finder.length >= waiter.length) {
-                waiter.push(socket.id);
+            if (waiting) {
                 socket.emit("side", { side: "O" });
-            } else {
-                finder.push(socket.id);
-                socket.emit("side", { side: "X" });
-            }
-            if (finder.indexOf(socket.id) !== -1) {
-                let find = Math.floor(Math.random()*waiter.length);
-                while (!io.sockets.sockets.has(waiter[find])) {
-                    find = Math.floor(Math.random()*waiter.length);
-                }
-                io.to(waiter[find]).emit("found", {opponent: socket.id});
-                io.to(socket.id).emit("found", {opponent: waiter[find]});
-                let room = socket.id + waiter[find];
+                let room = socket.id + waiting;
                 socket.join(room);
-                io.sockets.sockets.get(waiter[find]).join(room);
+                io.sockets.sockets.get(waiting).join(room);
+                io.to(waiting).emit("found", { opponent: socket.id });
+                socket.emit("found", { opponent: waiting });
                 io.to(room).emit("joined", { room });
-                finder.splice(finder.indexOf(socket.id), 1);
-                waiter.splice(find, 1);
+                waiting = null;
+            } else {
+                socket.emit("side", { side: "X" });
+                waiting = socket.id;
             }
         });
         socket.on("move", (data) => {
